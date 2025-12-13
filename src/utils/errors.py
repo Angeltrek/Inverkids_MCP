63a46d2354cd8506b4c0f43f2e9f7c81ddb530a6
Angeltrek <angeltrek007@gmail.com>
2025-12-13 13:46:56 -0600

    feat(connection): Database connection implemented

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -3,3 +3,9 @@ class ToolRoutingError(Exception):
     Raised when a tool call cannot be routed safely.
     """
     pass
+
+class DatabaseConnectionError(Exception):
+    """
+    Raised when there is a database connection issue.
+    """
+    pass
